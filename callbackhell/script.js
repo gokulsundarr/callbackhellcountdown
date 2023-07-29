6fe703835a9b1387f1d707befc004aa4b6e0975d
gokulsundarr <133556658+gokulsundarr@users.noreply.github.com>
2023-07-29 19:28:19 +0530

    callbackhell countdown

---
+++ callbackhell/script.js
@@ -0,0 +1,52 @@
+
+let days=10;
+let day =document.getElementById("independence");
+var call="HAPPY INDEPENDENCE DAY";
+setTimeout(()=>{
+    day.innerHTML = days--;
+    setTimeout(()=>{
+        day.innerHTML = days--;
+        setTimeout(()=>{
+            day.innerHTML = days--;
+            setTimeout(()=>{
+                day.innerHTML = days--;
+                setTimeout(()=>{
+                    day.innerHTML = days--;
+                    setTimeout(()=>{
+                        day.innerHTML = days--;
+                        setTimeout(()=>{
+                            day.innerHTML = days--;
+                            setTimeout(()=>{
+                                day.innerHTML = days--;
+                                setTimeout(()=>{
+                                    day.innerHTML = days--;
+                                    setTimeout(()=>{
+                                        day.innerHTML = days--;
+                                        setTimeout(()=>{
+                                    
+                                            day.innerHTML = days--;
+                                            day.innerHTML=call;
+                                            
+                                        },1000)
+                                    },1000)
+                                                                        
+                                },1000)
+                                                                
+                            },1000)
+                                                        
+                        },1000)
+                                                
+                    },1000)
+                                        
+                },1000)
+                                
+            },1000)
+                        
+        },1000)
+                
+    },1000)
+    
+},1000);
+
+
+
